5c23c929b59d451036715b5e283eea0cb0d4086b
Arturo Aliaga <jaav2801@gmail.com>
2025-11-20 12:04:11 -0400

    feat: update API base URL to production endpoint

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://api.sw2ficct.lat/api',
   headers: {
     'Content-Type': 'application/json',
   },
